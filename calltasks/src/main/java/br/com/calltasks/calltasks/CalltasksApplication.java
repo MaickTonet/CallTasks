69ecf39fefd0877c7ee15391fff8850551ae2ba7
Maick Tonet <maicktonet4@gmail.com>
2023-11-08 19:27:28 -0300

    arquivo maven com as dependências

---
+++ calltasks/src/main/java/br/com/calltasks/calltasks/CalltasksApplication.java
@@ -0,0 +1,13 @@
+package br.com.calltasks.calltasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalltasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalltasksApplication.class, args);
+	}
+
+}
---
+++ calltasks/src/test/java/br/com/calltasks/calltasks/CalltasksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calltasks.calltasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalltasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

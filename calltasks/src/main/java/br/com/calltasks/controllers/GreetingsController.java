7f3e8c3faa3e117e5408599e0a6d34428f3524f1
Maick Tonet <maicktonet4@gmail.com>
2023-11-21 22:06:24 -0300

    protótipo dos endpoints

--- calltasks/src/main/java/br/com/calltasks/controllers/GreetingsController.java
+++ calltasks/src/main/java/br/com/calltasks/controllers/GreetingsController.java
@@ -4,8 +4,16 @@ import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.calltasks.model.Usuario;
@@ -17,12 +25,46 @@ public class GreetingsController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@GetMapping(path = "/list")
-	public List<Usuario> listarUsuarios() {
-		return usuarioRepository.findAll();
+	public ResponseEntity<List<Usuario>> listarUsuarios() {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "salvar")
+	@ResponseBody
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+		Usuario user = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping(value = "delete")
+	@ResponseBody
+	public ResponseEntity<String> delete(@RequestParam String iduser) {
+		usuarioRepository.deleteById(iduser);
+		return new ResponseEntity<String>("Usuario excluido com sucesso", HttpStatus.OK);
+	}
+
+	@GetMapping(value = "buscaruserid")
+	@ResponseBody
+	public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") String iduser) {
+		Usuario usuario = usuarioRepository.findById(iduser).get();
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+	}
+
+	@PutMapping(value = "atualizar")
+	@ResponseBody
+	public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) {
+		if (usuario.getCpfUsuario() == null) {
+			return new ResponseEntity<String>("Id não informado para atualizar", HttpStatus.OK);
+		}
+		Usuario user = usuarioRepository.saveAndFlush(usuario);
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
 	}
 	
+	
+
 	@GetMapping(path = "/create")
 	public String testeGravar() {
 		Usuario usuario = new Usuario();
@@ -32,7 +74,6 @@ public class GreetingsController {
 		usuario.setDataNascimento(new Date(0));
 		usuarioRepository.save(usuario);
 		return "Gravado";
-		
-		
+
 	}
 }
